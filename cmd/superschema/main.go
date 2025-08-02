dde88e2d34a5a9a51198c28ddccbb0744bff4222
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 15:06:44 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/superschema/main.go
@@ -0,0 +1,23 @@
+// cmd/superschema/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"superschema/internal/superschema"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := superschema.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
